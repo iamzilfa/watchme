a0285cf85ca5a87bd1e8c06a0b2ec5b1cad14c3d
iamzilfa <iamzilfa@gmail.com>
2022-12-17 11:13:07 +0200

    new color

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline bg-red-500">
       It works fine!
     </h1>
   )
